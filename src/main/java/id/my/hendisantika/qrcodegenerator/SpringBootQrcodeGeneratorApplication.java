a7d5cc8828a3e33f1733ab9e9e05812cc59a6e43
hendisantika <hendisantika@yahoo.co.id>
2024-01-16 07:35:07 +0700

    Create starter project 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/id/my/hendisantika/qrcodegenerator/SpringBootQrcodeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.qrcodegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQrcodeGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootQrcodeGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/qrcodegenerator/SpringBootQrcodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.qrcodegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQrcodeGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

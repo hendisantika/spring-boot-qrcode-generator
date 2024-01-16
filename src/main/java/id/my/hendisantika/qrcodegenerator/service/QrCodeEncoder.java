d0dbbe1dd841b3787c9ac9ac3d0330f236ee270d
hendisantika <hendisantika@yahoo.co.id>
2024-01-16 08:02:12 +0700

    Add generateImageAsBase64 in QrCodeEncoder Service 🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/id/my/hendisantika/qrcodegenerator/service/QrCodeEncoder.java
+++ src/main/java/id/my/hendisantika/qrcodegenerator/service/QrCodeEncoder.java
@@ -1,5 +1,10 @@
 package id.my.hendisantika.qrcodegenerator.service;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import id.my.hendisantika.qrcodegenerator.model.QrCodeEmail;
 import id.my.hendisantika.qrcodegenerator.model.QrCodeEmailParser;
 import id.my.hendisantika.qrcodegenerator.model.QrCodeEvent;
@@ -16,7 +21,17 @@ import id.my.hendisantika.qrcodegenerator.model.QrCodeUrlParser;
 import id.my.hendisantika.qrcodegenerator.model.QrCodeVCard;
 import id.my.hendisantika.qrcodegenerator.model.QrCodeVCardParser;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Base64Utils;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -64,5 +79,46 @@ public class QrCodeEncoder {
     public QrCodeProcessingResult generateQrCodeVCard(QrCodeVCard qrCodeVCard) {
         String extracted = new QrCodeVCardParser(qrCodeVCard).parse();
         return this.generateImageAsBase64(extracted);
+    }
+
+    private QrCodeProcessingResult generateImageAsBase64(String textToBeEncoded) {
+        QrCodeProcessingResult result = new QrCodeProcessingResult();
+        result.setEncodedText(textToBeEncoded);
+        String imageText = "";
+        int size = 250;
+        String fileType = "png";
+        try {
+            Map<EncodeHintType, Object> hintMap = createHintMap();
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            BitMatrix bitMatrix = qrCodeWriter.encode(textToBeEncoded, BarcodeFormat.QR_CODE, size, size, hintMap);
+            int width = bitMatrix.getWidth();
+            int height = bitMatrix.getHeight();
+            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            image.createGraphics();
+            Graphics2D graphics = (Graphics2D) image.getGraphics();
+            graphics.setColor(Color.WHITE);
+            graphics.fillRect(0, 0, width, height);
+            graphics.setColor(Color.BLACK);
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < width; j++) {
+                    if (bitMatrix.get(i, j)) {
+                        graphics.fillRect(i, j, 1, 1);
+                    }
+                }
+            }
+            String fileName = UUID.randomUUID().toString();
+            File myFile = File.createTempFile(fileName, "." + fileType);
+            ImageIO.write(image, fileType, myFile);
+            byte[] bytes = FileUtils.readFileToByteArray(myFile);
+            imageText = "data:image/png;base64," + Base64Utils.encodeToString(bytes);
+            result.setImage(imageText);
+        } catch (WriterException | IOException e) {
+            String msg = "Processing QR code failed.";
+            log.error(msg, e);
+            result.setErrorMessage(msg);
+        }
+        log.info("QR Code for text {} was successfully created.", textToBeEncoded);
+        result.setSuccessMessage("QR Code was successfully created.");
+        return result;
     }
 }

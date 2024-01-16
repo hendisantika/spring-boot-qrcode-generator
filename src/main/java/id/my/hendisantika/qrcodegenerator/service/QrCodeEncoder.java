7a3ffd2863c944077ee4de5f3bfb11ee2afc987b
hendisantika <hendisantika@yahoo.co.id>
2024-01-16 08:00:12 +0700

    Add QrCodeEncoder Service 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/id/my/hendisantika/qrcodegenerator/service/QrCodeEncoder.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.qrcodegenerator.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-qrcode-generator
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/16/24
+ * Time: 07:59
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+@Slf4j
+public class QrCodeEncoder {
+}

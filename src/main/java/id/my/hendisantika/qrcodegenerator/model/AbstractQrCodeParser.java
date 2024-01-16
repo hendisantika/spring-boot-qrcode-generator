c23b497d4f2a1ff9b83dd56b420021383cf30d82
hendisantika <hendisantika@yahoo.co.id>
2024-01-16 07:52:01 +0700

    Add AbstractQrCodeParser Model 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/id/my/hendisantika/qrcodegenerator/model/AbstractQrCodeParser.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.qrcodegenerator.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-qrcode-generator
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/16/24
+ * Time: 07:51
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class AbstractQrCodeParser {
+    public abstract String parse();
+}

a2b731e6f4f12ce68cf3a50d3746b5996374cf86
hendisantika <hendisantika@yahoo.co.id>
2024-01-17 09:07:30 +0700

    Add thatParseUrlWithBlanksPasses in QrCodeUrlParserTest🫸🌀✏️📗 :octocat:🐧🐳

--- src/test/java/id/my/hendisantika/qrcodegenerator/model/QrCodeUrlParserTest.java
+++ src/test/java/id/my/hendisantika/qrcodegenerator/model/QrCodeUrlParserTest.java
@@ -21,4 +21,11 @@ class QrCodeUrlParserTest {
         QrCodeUrl qrCodeUrl = new QrCodeUrl("http://www.google.com");
         assertEquals(expected, new QrCodeUrlParser(qrCodeUrl).parse());
     }
+
+    @Test
+    public void thatParseUrlWithBlanksPasses() {
+        String expected = "http://www.google.com?value=My%20Site";
+        QrCodeUrl qrCodeUrl = new QrCodeUrl("http://www.google.com?value=My%20Site");
+        assertEquals(expected, new QrCodeUrlParser(qrCodeUrl).parse());
+    }
 }

cc18d9de2e5232fe0c21ae84e1afe7316182020d
hendisantika <hendisantika@yahoo.co.id>
2024-01-17 08:55:20 +0700

    Add TestUtils🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/test/java/id/my/hendisantika/qrcodegenerator/util/TestUtils.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.qrcodegenerator.util;
+
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-qrcode-generator
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/17/24
+ * Time: 08:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestUtils {
+    private static final LocalValidatorFactoryBean LOCAL_VALIDATOR_FACTORY_BEAN = createLocalValidatorFactoryBean();
+
+    private TestUtils() {
+    }
+}

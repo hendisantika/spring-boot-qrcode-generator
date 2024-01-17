8ba226a2f6622cbc50e304b275bc0afd4ad120a9
hendisantika <hendisantika@yahoo.co.id>
2024-01-17 09:07:06 +0700

    Add QrCodeUrlParserTest🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/test/java/id/my/hendisantika/qrcodegenerator/model/QrCodeUrlParserTest.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.qrcodegenerator.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-qrcode-generator
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/17/24
+ * Time: 09:06
+ * To change this template use File | Settings | File Templates.
+ */
+class QrCodeUrlParserTest {
+    @Test
+    public void thatParseUrlPasses() {
+        String expected = "http://www.google.com";
+        QrCodeUrl qrCodeUrl = new QrCodeUrl("http://www.google.com");
+        assertEquals(expected, new QrCodeUrlParser(qrCodeUrl).parse());
+    }
+}

ef57455107a5ec2225d4efeb0b0425bad030af3a
hendisantika <hendisantika@yahoo.co.id>
2024-01-17 08:51:26 +0700

    Add ApplicationPropertiesTest🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/test/java/id/my/hendisantika/qrcodegenerator/config/ApplicationPropertiesTest.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.qrcodegenerator.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-qrcode-generator
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/17/24
+ * Time: 08:51
+ * To change this template use File | Settings | File Templates.
+ */
+@SpringBootTest
+public class ApplicationPropertiesTest {
+
+    @Autowired
+    ApplicationProperties applicationProperties;
+
+}

344d2d75beb628d674ac869f310b0cd63aa7d34d
hendisantika <hendisantika@yahoo.co.id>
2024-01-17 09:16:47 +0700

    Add QrCodeEncoderTest 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/test/java/id/my/hendisantika/qrcodegenerator/service/QrCodeEncoderTest.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.qrcodegenerator.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-qrcode-generator
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/17/24
+ * Time: 09:16
+ * To change this template use File | Settings | File Templates.
+ */
+@SpringBootTest
+class QrCodeEncoderTest {
+    @Autowired
+    private QrCodeEncoder qrCodeEncoder;
+}
